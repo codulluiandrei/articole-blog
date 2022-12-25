b8738ead5f7f133502c2b1d289119045a97ad3d4
Andrew <frintu.andrei07@gmail.com>
2022-12-25 18:57:12 +0200

    sursa py @ generare-numere-fibonacci

---
+++ generare-numere-fibonacci/fisier.py
@@ -0,0 +1,12 @@
+# afiseaza primele n numere fibonacci
+def generare_fibonacci(a):
+    a = b = 1
+    print(a)
+    print(b)
+    for i in range (n):
+        c = a + b
+        print(c, " ")
+        a = b
+        b = c
+n = int(input())
+generare_fibonacci(n)
