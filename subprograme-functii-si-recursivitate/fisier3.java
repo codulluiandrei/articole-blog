2fef1f2d74c9239fab822aa1aff6afe8eced18be
Andrew <frintu.andrei07@gmail.com>
2023-05-13 13:09:55 +0300

    sursa java @ subprograme-functii-si-recursivitate

---
+++ subprograme-functii-si-recursivitate/fisier3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Tutorial {
+	public static int suma(int a, int b) {
+		return a + b;
+	}
+	public static void main(String[] args) {
+        	Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.println(suma(a, b));
+	}
+}
