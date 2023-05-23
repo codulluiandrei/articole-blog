8885eae3ba2e464abe14ea9b1a975ad82a778504
Andrew <frintu.andrei07@gmail.com>
2023-05-23 22:23:57 +0300

    sursa java @ subprograme-functii-si-recursivitate

---
+++ subprograme-functii-si-recursivitate/fisier9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Tutorial {
+	public static int fact(int n) {
+		if (n == 0)
+			return 1;
+		else 
+			return fact(n - 1) * n;
+	}
+	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(fact(n));
+	}
+}
